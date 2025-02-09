86cffa63c85c8d237614414b1a72293ac44f2163
Jamesh6210 <adidassport2016@gmail.com>
2025-02-09 16:03:13 +0000

    Auto-updated guide.xml

--- epg/run_epg.bat
+++ epg/run_epg.bat
@@ -1,3 +1,3 @@
 @echo off
 cd C:\Users\James\Documents\M3U-Playlist\epg
-npm run grab --- --channels=C:\Users\James\Documents\M3U-Playlist\Main-playlists\epg2.xml --maxConnections=10 --days=7
+npm run grab --- --channels=C:\Users\James\Documents\M3U-Playlist\Main-playlists\epg2.xml --maxConnections=10 --days=5
