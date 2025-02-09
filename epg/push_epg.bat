e09bd98a20b63c574bc08300cf5cf05a8e924d21
Jamesh6210 <adidassport2016@gmail.com>
2025-02-09 16:24:55 +0000

    epg update

--- epg/push_epg.bat
+++ epg/push_epg.bat
@@ -1,5 +1,14 @@
 @echo off
-cd C:\Users\James\Documents\M3U-Playlist\Main-playlists
-git add guide.xml
+cd C:\Users\James\Documents\M3U-Playlist\epg
+
+:: Stage the changes for commit
+git add ../epg/guide.xml
+git add ../epg/run_epg.bat
+
+:: Commit the changes
 git commit -m "Auto-updated guide.xml"
+
+:: Push to GitHub
 git push origin main
+
+echo Pushed at %DATE% %TIME% >> epg_push_log.txt
